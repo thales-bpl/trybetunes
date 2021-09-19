fc5a2be5e48a8191d83670ac69d854aab48e389a
Thales Lima <thales.bplima@gmail.com>
2021-09-19 01:19:57 -0300

    refat req1: add Component Content

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,31 @@
+// O botão para entrar só deve ser habilitado caso o nome digitado tenha 3 ou mais caracteres.
+// Ao clicar no botão Entrar, utilize a função createUser da userAPI para salvar o nome digitado. A função createUser espera receber como argumento um objeto com as informações da pessoa
+
 import React, { Component } from 'react';
 
 class Login extends Component {
   render() {
     return (
-      <div>Login</div>
+      <>
+        <form>
+          <label htmlFor="login-name-input">
+            User:
+            <input
+              data-testid="login-name-input"
+              name=""
+              /* value={ name } */
+              /* onChange={ handleChange } */
+              type="text"
+            />
+          </label>
+        </form>
+        <button
+          data-testid="login-submit-button"
+          type="submit"
+        >
+          Entrar
+        </button>
+      </>
     );
   }
 }
