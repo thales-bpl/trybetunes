f20541c97cb4781445783046d933309ec851ec1c
Thales Lima <thales.bplima@gmail.com>
2021-10-11 22:48:07 -0300

    broken req10

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../Components/Loading';
+import MusicCard from '../Components/MusicCard';
 
 class Favorites extends Component {
-/*   constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       loading: true,
       favoriteTracks: [],
     };
-  } */
+    this.fetchFavoriteSongs = this.fetchFavoriteSongs.bind(this);
+  }
 
   componentDidMount() {
-    getFavoriteSongs();
+    this.fetchFavoriteSongs();
+  }
+
+  componentDidUpdate() {
+    this.fetchFavoriteSongs();
+  }
+
+  fetchFavoriteSongs() {
+    getFavoriteSongs().then((favoriteTracks) => {
+      this.setState({
+        loading: false,
+        favoriteTracks,
+      });
+    });
   }
 
   render() {
+    const { loading, favoriteTracks } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-favorites">Favorites</div>
+        { loading ? <Loading /> : '' }
+        <ul>
+          {favoriteTracks.map((track, index) => (
+            <li
+              key={ index }
+            >
+              <MusicCard
+                track={ track }
+              />
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
